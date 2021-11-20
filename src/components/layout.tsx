5332a9e7378b66197e6b1f9cf5d932148650f679
Jackson Oppenheim <jacksonop@gmail.com>
2021-11-20 14:10:50 -0600

    added css declaration added posts added blog page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import { navLink } from './layout.module.css';
 
@@ -7,21 +7,43 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div>
-      <title>{pageTitle}</title>
-      <nav className='w-screen bg-purple-400 py-2 mb-2'>
-        <ul className='flex gap-4 justify-center'>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <nav className='w-screen bg-purple-400 py-2 px-3 mb-2 flex justify-between'>
+        <header className="">{data.site.siteMetadata.title}</header>
+        <ul className='flex gap-4'>
           <li>
-            <Link to='/' className={navLink}>Home</Link>
+            <Link to='/' className={navLink}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to='/about' className={navLink}>About</Link>
+            <Link to='/about' className={navLink}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='/blog' className={navLink}>
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
       <main className='mx-auto max-w-lg'>
-        <h1 className="text-purple-500 text-5xl">{pageTitle}</h1>
+        <h1 className='text-purple-500 text-5xl'>{pageTitle}</h1>
         {children}
       </main>
     </div>
